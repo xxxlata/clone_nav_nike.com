47948f79ef534c53e85ac47f6a5f8a4b028964bf
bigtoe <xxxlata@naver.com>
2021-09-28 20:02:50 +0900

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+function HandlerWindowWidth() {
+    const innerwidth = window.innerWidth;
+    if (innerwidth < 1000) {
+      document.body.
+    } else {
+    
+    }
+  }
+  
+  window.addEventListener("resize", HandlerWindowWidth);
